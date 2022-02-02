c41969f22e704ca03c0bc0c1c482b16023841092
Matheus Alan <matheusalanpv@outlook.com>
2022-02-02 20:38:03 -0300

    pequena correcao ortografica

--- praticando-springboot/loja-virtual/src/main/java/br/com/lojaonline/controller/ProductController.java
+++ praticando-springboot/loja-virtual/src/main/java/br/com/lojaonline/controller/ProductController.java
@@ -23,22 +23,22 @@ public class ProductController {
     private DateUtil dateUtil;
 
     @PostMapping("/insertProduct")
-    public ResponseEntity<Product> updateProduct(@RequestBody Product product) {
+    public ResponseEntity<Product> insertProduct(@RequestBody Product product) {
         log.info(dateUtil.dataFormatada(LocalDateTime.now()).concat(" /insertProduct"));
         Product updateProduct = productRepository.save(product);
         return ResponseEntity.status(HttpStatus.OK).body(updateProduct);
     }
 
     @GetMapping("/allProducts")
-    public ResponseEntity<List<Product>> findAll() {
+    public ResponseEntity<List<Product>> findAllProducts() {
         log.info(dateUtil.dataFormatada(LocalDateTime.now()).concat(" /allProducts"));
         List<Product> findAll = productRepository.findAll();
         return ResponseEntity.status(HttpStatus.OK).body(findAll);
     }
 
-    @PutMapping("/alterById/{id}")
-    public ResponseEntity<Product> attProduct(@RequestBody Product product, @PathVariable Long id) {
-        log.info(dateUtil.dataFormatada(LocalDateTime.now()).concat(" /alterById/{id}"));
+    @PutMapping("/updateById/{id}")
+    public ResponseEntity<Product> updateProductById(@RequestBody Product product, @PathVariable Long id) {
+        log.info(dateUtil.dataFormatada(LocalDateTime.now()).concat(" /updateById/{id}"));
         return productRepository.findById(id)
                 .map(prod -> ResponseEntity.status(HttpStatus.OK).body(prod))
                 .orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).body(product));
