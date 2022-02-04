210e6b6b04c0da65ed6afae47cd2be0a3058e5b9
Matheus Alan <matheusalanpv@outlook.com>
2022-02-03 22:02:28 -0300

    adicionado novo jeito para captura de log por meio do objeto Logger/LogManager.getLogger(<nome da classe>.class) e adicionado o pacote service para comecar a estudar esse padrao

--- praticando-springboot/loja-virtual/src/main/java/br/com/lojaonline/controller/ProductController.java
+++ praticando-springboot/loja-virtual/src/main/java/br/com/lojaonline/controller/ProductController.java
@@ -2,8 +2,10 @@ package br.com.lojaonline.controller;
 
 import br.com.lojaonline.model.Product;
 import br.com.lojaonline.repository.ProductRepository;
+import br.com.lojaonline.service.ProductService;
 import br.com.lojaonline.util.DateUtil;
-import lombok.extern.log4j.Log4j2;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,17 +18,33 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
-@Log4j2
+//@Log4j2
 public class ProductController {
 
+    private static final Logger logger = LogManager.getLogger(ProductController.class);
     @Autowired
     private ProductRepository productRepository;
     @Autowired
+    private ProductService productService;
+    @Autowired
     private DateUtil dateUtil;
+
+    @GetMapping(path = "/listeTudo")
+    public ResponseEntity<List<Product>> listeTudo() {
+        productService.getProduct();
+        return new ResponseEntity<List<Product>>(HttpStatus.OK);
+    }
+
+    @PostMapping("/criarProduto")
+    public ResponseEntity<Product> criandoProduto(@RequestBody Product product) {
+        Product novoProduto = productService.createProduct(product);
+        return new ResponseEntity<Product>(HttpStatus.CREATED);
+    }
 
     @PostMapping("/insertProduct")
     public ResponseEntity<Product> insertProduct(@RequestBody Product product) {
-        log.info(dateUtil.dataFormatada(LocalDateTime.now()).concat(" /insertProduct"));
+        logger.info(dateUtil.dataFormatada(LocalDateTime.now()).concat(" /insertProduct"));
+//        log.info(dateUtil.dataFormatada(LocalDateTime.now()).concat(" /insertProduct"));
         Product updateProduct = productRepository.save(product);
         return ResponseEntity.status(HttpStatus.OK).body(updateProduct);
 //        return new ResponseEntity<Product>(HttpStatus.OK);
@@ -34,7 +52,8 @@ public class ProductController {
 
     @GetMapping("/allProducts")
     public ResponseEntity<List<Product>> findAllProducts() {
-        log.info(dateUtil.dataFormatada(LocalDateTime.now()).concat(" /allProducts"));
+        logger.info(dateUtil.dataFormatada(LocalDateTime.now()).concat(" /allProducts"));
+//        log.info(dateUtil.dataFormatada(LocalDateTime.now()).concat(" /allProducts"));
         List<Product> findAll = productRepository.findAll();
         return ResponseEntity.status(HttpStatus.OK).body(findAll);
 //        return new ResponseEntity<List<Product>>(HttpStatus.OK);
@@ -42,7 +61,8 @@ public class ProductController {
 
     @GetMapping(path = "/findById/{id}")
     public ResponseEntity<Optional<Product>> findById(@PathVariable Long id) {
-        log.info(dateUtil.dataFormatada(LocalDateTime.now()).concat(" /findById"));
+        logger.info(dateUtil.dataFormatada(LocalDateTime.now()).concat(" /findById"));
+//        log.info(dateUtil.dataFormatada(LocalDateTime.now()).concat(" /findById"));
         try {
             Optional<Product> findId = productRepository.findById(id);
             return ResponseEntity.ok().body(findId);
@@ -54,7 +74,8 @@ public class ProductController {
 
     @PutMapping("/updateById/{id}")
     public ResponseEntity<Product> updateProductById(@PathVariable Long id, @RequestBody Product newProduct) {
-        log.info(dateUtil.dataFormatada(LocalDateTime.now()).concat(" /updateById/{id}"));
+        logger.info(dateUtil.dataFormatada(LocalDateTime.now()).concat(" /updateById"));
+//        log.info(dateUtil.dataFormatada(LocalDateTime.now()).concat(" /updateById/{id}"));
         return productRepository.findById(id)
                 .map(productChanged -> {
                     productChanged.setName(newProduct.getName());
@@ -67,7 +88,8 @@ public class ProductController {
 
     @DeleteMapping(path = "/deleteById/{id}")
     public ResponseEntity<Optional<Product>> deleteById(@PathVariable Long id) {
-        log.info(dateUtil.dataFormatada(LocalDateTime.now()).concat(" /deleteById"));
+        logger.info(dateUtil.dataFormatada(LocalDateTime.now()).concat(" /deleteById"));
+//        log.info(dateUtil.dataFormatada(LocalDateTime.now()).concat(" /deleteById"));
         try {
             productRepository.deleteById(id);
             return new ResponseEntity<Optional<Product>>(HttpStatus.OK);
